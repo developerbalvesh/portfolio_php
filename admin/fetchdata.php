ebe5dfc61f63750b17ae42e6d6a2311c9f999b84
developerbalvesh <developer.balvesh@gmail.com>
2024-04-08 21:35:41 +0530

    first commit

---
+++ admin/fetchdata.php
@@ -0,0 +1,16 @@
+<?php
+$name="Balvesh Puramkar";
+$ccompany="PSK Technologies Pvt. Ltd.";
+$design="Web Developer";
+$longIntro="I’m a Web developer at  ".$ccompany." I love making websites that people enjoy using. I know a bunch of coding languages, framework, and libraries like ReactJS, PhP, Bootstrap, javascript, jQuery, CSS and HTML. I'm good at making things look nice and work well. I like working with others to come up with cool ideas and turn them into reality. I'm always learning new stuff to stay on top of things in the web world.";
+$birthdate="24-03-2000";
+$age="24";
+$city="Nagpur";
+$degree="Graduated in CSE";
+$study="RTM Nagpur University";
+$mail="developer.balvesh@gmail.com";
+$website="www.devopsbalvesh.com";
+$phone="+91 8208940419";
+
+
+?>
